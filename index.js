32c75352fb3edd96e87f65e82156046ea557ae99
REJOINT <brecab120@gmail.com>
2023-02-16 12:34:16 -0500

    Adding 404 page view

--- index.js
+++ index.js
@@ -5,16 +5,19 @@ const express = require('express')
 
 const app = express()
 
-app.set('views', __dirname + '/views')
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+//app.set('views', __dirname + '/views')
 
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('home')
 })
 
 app.get('*', (req, res) => {
-    res.status(404).send('<h1>404 Page</h1>')
+    res.render('error404')
 })
 
 app.listen(process.env.PORT)
---
+++ views/home.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+
+const Def = require('./layouts/default')
+
+function home () {
+    return (
+        <Def>
+            <main>
+                <h1>HOME</h1>
+            </main>
+        </Def>
+    )
+}
+
+module.exports = home
+
