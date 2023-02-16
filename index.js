5d147d80c7d8075d11c1950aeba1e77746486f7b
REJOINT <brecab120@gmail.com>
2023-02-16 14:46:04 -0500

    Adding styles to REST-Rant

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ const express = require('express')
 
 const app = express()
 
+// Express settings
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
 
 //app.set('views', __dirname + '/views')
 
+// Controllers and Routes
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
@@ -21,4 +24,5 @@ app.get('*', (req, res) => {
     res.render('error404')
 })
 
+// Listen for connections
 app.listen(process.env.PORT)
