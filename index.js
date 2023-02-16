6bbd903df4249543d4d05e8288a2ed9ebeea8f7a
REJOINT <brecab120@gmail.com>
2023-02-16 10:42:23 -0500

    Adding default view for layout

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const express = require('express')
 
 const app = express()
 
+app.set('views', __dirname + '/views')
+
 app.use('/places', require('./controllers/places'))
 
 app.get('/', (req, res) => {
