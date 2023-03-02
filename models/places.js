9ea9b73986c11168b9358aa28872e5701908529c
REJOINT <brecab120@gmail.com>
2023-03-02 17:43:30 -0500

    Updates

--- models/places.js
+++ models/places.js
@@ -1,16 +1,12 @@
-module.exports = [{
-    name: 'H-Thai-ML',
-    city: 'Tokyo',
-    state: 'JP',
-    cuisines: 'Japanese',
-    pic: '/images/ambiance.jpg'
-    // Photo by <a href="https://unsplash.com/ko/@ulysse_pcl?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Ulysse Pointcheval</a> on <a href="https://unsplash.com/photos/1WmlAiYgnoI?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+const mongoose = require('mongoose')
 
-}, {
-    name: 'Coding Cat Cafe',
-    city: 'Rhinebeck',
-    state: 'NY',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/bakery.jpg'
-    // Photo by <a href="https://unsplash.com/@thattravelblog?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Dave Weatherall</a> on <a href="https://unsplash.com/photos/vIDwXIkbkOk?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
-}]
+const placeSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    pic: String,
+    cuisines: { type: String, required: true },
+    city: { type: String, default: 'Anytown' },
+    state: { type: String, default: 'USA' }, 
+    founded: Number,
+})
+
+module.exports = mongoose.model('Place', placeSchema)
