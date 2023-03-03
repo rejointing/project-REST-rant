0d62e8901bb89f244ff004bf1b6390acc057d418
REJOINT <brecab120@gmail.com>
2023-03-02 21:15:27 -0500

    Updates

--- models/index.js
+++ models/index.js
@@ -2,8 +2,9 @@ require('dotenv').config()
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGO_URI, {
-    // useNewParser: true,
-    // useUnifiedTypology: true
+    useNewParser: true,
+    useUnifiedTypology: true
 })
 
 module.exports.Place = require('./places')
+module.exports.Comment = require('./comment')
