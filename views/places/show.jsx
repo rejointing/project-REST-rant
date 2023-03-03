10174d174fa5851d52a9d33f6417cc5cc99ff50f
REJOINT <brecab120@gmail.com>
2023-03-02 20:36:28 -0500

    Updates

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -6,6 +6,23 @@ function show (data) {
     return (
         <Def>
             <main>
+                <div className="row">
+                    <div className="col-sm-6">
+                        <img src={data.place.pic} alt={data.place.name} />
+                        <h3>
+                            Located in {data.place.city}, {data.place.state}
+                        </h3>
+                    </div>
+                    <div className="col-sm-6">
+                    <h2>
+                        Description
+                    </h2>
+                    <h3> 
+                        {data.place.showEstablished()}
+                    </h3>
+                    <h4>
+                        Serving {data.place.cuisines}
+                    </h4>
                 <h1>{ data.place.name }</h1>
                 <a href={`/places/${data.id}/edit`} className="btn btn-warning">
                     Edit
@@ -15,6 +32,8 @@ function show (data) {
                         Delete
                     </button>
                 </form>
+                </div>
+                </div>
             </main>
         </Def>
     )
